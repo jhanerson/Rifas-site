018c27b3cf393aa2e4dec9a8ed12567f9c50ebd9
Mohacking <96668230+jhanerson@users.noreply.github.com>
2023-07-25 20:54:00 -0500

    V-1

--- site/code.js
+++ site/code.js
@@ -4,96 +4,52 @@ window.onload = function () {
     $('body').removeClass('no-scroll')
 }
 
-// Esperar a que el DOM termine de cargar
-document.addEventListener("DOMContentLoaded", function () {
-    // Obtener el formulario de venta por su ID
-    var ventaForm = document.getElementById("ventaForm");
+// Obtener el elemento del círculo
+const circle = document.getElementById("circle");
 
-    // Agregar un evento de escucha para el evento de envío del formulario
-    ventaForm.addEventListener("submit", function (event) {
-        event.preventDefault(); // Evitar que el formulario se envíe
+// Área del juego
+const gameArea = document.querySelector(".game-area");
 
-        // Obtener los valores de los campos del formulario
-        var producto = document.getElementById("producto").value;
-        var cantidad = document.getElementById("cantidad").value;
-        var precio = document.getElementById("precio").value;
+// Movimiento del círculo
+let circleX = 0;
+let circleY = 0;
 
-        // Calcular el total de la venta
-        var total = cantidad * precio;
+// Tamaño del área del juego
+const gameWidth = gameArea.offsetWidth;
+const gameHeight = gameArea.offsetHeight;
 
-        // Obtener la fecha actual
-        var fecha = new Date().toLocaleDateString();
+// Función para actualizar la posición del círculo
+function updatePosition() {
+    circle.style.left = circleX + "px";
+    circle.style.bottom = circleY + "px";
+}
 
-        // Crear una nueva fila para la tabla de ventas
-        var fila = document.createElement("tr");
+// Función para manejar las teclas presionadas
+document.addEventListener("keydown", function (event) {
+    const step = 10; // Cantidad de píxeles para moverse
 
-        // Crear celdas para cada dato de la venta
-        var celdaProducto = document.createElement("td");
-        celdaProducto.textContent = producto;
-        var celdaCantidad = document.createElement("td");
-        celdaCantidad.textContent = cantidad;
-        var celdaPrecio = document.createElement("td");
-        celdaPrecio.textContent = precio;
-        var celdaTotal = document.createElement("td");
-        celdaTotal.textContent = total;
-        var celdaFecha = document.createElement("td");
-        celdaFecha.textContent = fecha;
+    switch (event.key) {
+        case "ArrowUp":
+            if (circleY < gameHeight - circle.offsetHeight) {
+                circleY += step;
+            }
+            break;
+        case "ArrowDown":
+            if (circleY > 0) {
+                circleY -= step;
+            }
+            break;
+        case "ArrowLeft":
+            if (circleX > 0) {
+                circleX -= step;
+            }
+            break;
+        case "ArrowRight":
+            if (circleX < gameWidth - circle.offsetWidth) {
+                circleX += step;
+            }
+            break;
+    }
 
-        // Agregar las celdas a la fila
-        fila.appendChild(celdaProducto);
-        fila.appendChild(celdaCantidad);
-        fila.appendChild(celdaPrecio);
-        fila.appendChild(celdaTotal);
-        fila.appendChild(celdaFecha);
-
-        // Obtener la tabla de ventas por su clase
-        var tablaVentas = document.querySelector(".table");
-
-        // Agregar la fila a la tabla
-        tablaVentas.appendChild(fila);
-
-        // Limpiar los campos del formulario
-        ventaForm.reset();
-    });
-
-    // Obtener el botón de "Eliminar producto" por su tipo
-    var eliminarProductoBtn = document.querySelector("button[type='button']");
-
-    // Agregar un evento de escucha para el evento de clic en el botón
-    eliminarProductoBtn.addEventListener("click", function () {
-        // Obtener la última fila de la tabla de ventas
-        var ultimaFila = document.querySelector(".table tr:last-child");
-
-        // Verificar si hay filas en la tabla
-        if (ultimaFila) {
-            // Eliminar la última fila de la tabla
-            ultimaFila.remove();
-        }
-    });
-});
-
-
-// Espera a que la página se cargue completamente
-window.addEventListener('load', function () {
-    // Obtiene el botón de exportar
-    var exportButton = document.getElementById('exportButton');
-
-    // Agrega un evento de clic al botón de exportar
-    exportButton.addEventListener('click', function () {
-        // Obtiene el elemento de la tabla
-        var table = document.querySelector('.table');
-
-        // Convierte la tabla a una imagen utilizando html2canvas
-        html2canvas(table).then(function (canvas) {
-            // Crea un elemento de enlace
-            var link = document.createElement('a');
-            link.download = 'tabla_ventas.png'; // Nombre del archivo de imagen descargado
-
-            // Convierte el lienzo a una URL de datos
-            link.href = canvas.toDataURL('image/png');
-
-            // Activa el clic en el enlace para iniciar la descarga
-            link.click();
-        });
-    });
+    updatePosition();
 });
